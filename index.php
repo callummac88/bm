cb8d4bbb994b88be9fe9e461e4a4bf53e44c61ca
Callum Macdonald <callum@redevolution.com>
2014-07-31 16:19:05 +0100

    added meta descriptions
    Signed-off-by: Callum Macdonald <callum@redevolution.com>

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-<title>Brian Mutch - Home</title>
+<title>Brian Mutch - Chef | Catering | Demo's</title>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<meta http-equiv="description" content="page description" />
+<meta name="description" content="Brian Mutch, Freelance Chef, Cookery Demos, Catering">
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>    
 <script src="assets/js/bootstrap.min.js"></script>    
@@ -19,5 +19,16 @@
 
 <?php include("includes/constants/footer.html");?>
 
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-53374542-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </body>
 </html>
